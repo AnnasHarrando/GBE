2f563d9cd9e3b85ab05819c6dfae2038f79ec503
annas <a.harrando@student.vu.nl>
2022-05-13 03:02:03 +0200

    Parts of MBC1 supported

--- src/bus.cpp
+++ src/bus.cpp
@@ -2,46 +2,40 @@
 
 uint8_t bus::read(uint16_t addr) {
         if (addr < 0x4000) {
-            //ROM Data
             return emu_read(addr,CART);
         }
         if (addr < 0x8000) {
-            //ROM Data
             return emu_read(addr,CART_BANK);
         }
         else if (addr < 0xA000) {
-            //Char/Map Data
             return emu_read(addr,VRAM);
         }
         else if (addr < 0xC000) {
-            //Cartridge RAM
             return emu_read(addr,RAM_BANK);
         }
         else if (addr < 0xE000) {
-            //WRAM (Working RAM)
             return emu_read(addr,WRAM);
         }
         else if (addr < 0xFE00) {
-            //reserved echo ram...
             return 0;
         }
         else if (addr < 0xFEA0) {
-            //OAM
-            return emu_read(addr,OAM);
+            if(dma_active()) {
+                printf("test\n");
+                return 0xFF;
+            }
+            else return emu_read(addr,OAM);
         }
         else if (addr < 0xFF00) {
-            //reserved unusable...
             return 0;
         }
         else if (addr < 0xFF80) {
-            //IO Registers...
             return emu_read(addr,IO);
         }
         else if(addr < 0xFFFF){
             return emu_read(addr,HRAM);
         }
         else if(addr == 0xFFFF){
-            //  return
             return get_ie_register();
         }
         printf("No bus addr %04X", addr);
@@ -50,47 +44,39 @@ uint8_t bus::read(uint16_t addr) {
 }
 
 void bus::write(uint16_t addr, uint8_t val) {
-    //printf("Writing to %04X\n\n", addr);
     if (addr < 0x4000) {
-        //ROM Data
         emu_write(addr, val,CART);
     }
     if (addr < 0x8000) {
-        //ROM Data
         //emu_write(addr, val, CART_BANK);
     }
     else if (addr < 0xA000) {
-        //Char/Map Data
         emu_write(addr, val,VRAM);
     }
     else if (addr < 0xC000) {
-        //EXT-RAM
         emu_write(addr, val,RAM_BANK);
     }
     else if (addr < 0xE000) {
-        //WRAM
         emu_write(addr, val,WRAM);
     }
     else if (addr < 0xFE00) {
-        //reserved echo ram
+
     }
     else if (addr < 0xFEA0) {
-        //OAM
+
         if(dma_active()) return;
         emu_write(addr, val,OAM);
     }
     else if (addr < 0xFF00) {
-        //unusable reserved
+
     }
     else if (addr < 0xFF80) {
-        //IO Registers...
         emu_write(addr, val,IO);
     }
     else if (addr < 0xFFFF) {
         emu_write(addr, val,HRAM);
     }
     else if(addr == 0xFFFF){
-        //CPU SET ENABLE REGISTER
         set_ie_register(val);
     }
 }
